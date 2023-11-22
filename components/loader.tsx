092aca78049f6fdd5a25cee5efff47498a0dcc31
dyanix <dyanixdhawale@gmail.com>
2023-11-22 13:02:09 +0530

    c

--- components/loader.tsx
+++ components/loader.tsx
@@ -6,7 +6,7 @@ export const Loader = () => {
       <div className="w-10 h-10 relative animate-spin">
         <Image
           alt="Logo"
-          src="/oLio.png"
+          src="/olio.png"
           fill
         />
       </div>
