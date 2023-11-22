7519bf5a0b0bae50520e244790f5a266110f96da
dyanix <dyanixdhawale@gmail.com>
2023-11-22 23:24:52 +0530

    c

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,17 +3,19 @@ import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "./mobile-sidebar"
 import { ModeToggle } from "./toggle";
 const NavBar = () => {
- 
+
     return (
         <div className="flex items-center p-4">
-           <MobileSidebar/>
-            <div className="flex w-full justify-end space-x-7 ">
-                <UserButton afterSignOutUrl="/" />
-                <ModeToggle/>
+            <MobileSidebar />
+            <div className="flex w-full  justify-end space-x-7 ">
+                <div className="mt-1">
+                    <UserButton afterSignOutUrl="/" />
+                </div>
+                <ModeToggle />
             </div>
 
-            
-            
+
+
         </div>
 
     )
